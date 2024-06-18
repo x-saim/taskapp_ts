bea7f61b09d22a7d44ed943d6e06fc7106126972
x-saim <saimxamir@gmail.com>
2024-06-18 01:00:53 -0400

    set up prelim task form with type alias

--- vite-project/src/tasks.ts
+++ vite-project/src/tasks.ts
@@ -1,1 +1,28 @@
 console.log('tasks');
+
+// Access and store DOM elements with type safety.
+const taskForm = document.querySelector<HTMLFormElement>('.form');
+const formInput = document.querySelector<HTMLInputElement>('.form-input');
+const taskListElement = document.querySelector<HTMLUListElement>('.list');
+
+// Type Alias
+type Task = {
+  description: string;
+  isCompleted: boolean;
+}
+
+const tasks: Task[] = [];
+
+taskForm?.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const taskDescription = formInput?.value;
+  if (taskDescription) {
+  console.log(taskDescription);
+
+    formInput.value = '';
+  } else {
+    alert('Please enter a task description.');
+  }
+
+})
